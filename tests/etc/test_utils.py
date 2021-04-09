27a674a1dd18c2bc996413c6979c8fffc1707956
daveusa31 <81916132+daveusa31@users.noreply.github.com>
2021-04-10 02:10:47 +0300

    Write new unit test

--- tests/etc/test_utils.py
+++ tests/etc/test_utils.py
@@ -6,3 +6,8 @@ def test_sorted_dict():
     un_sorted_dict = {"tye": 7, "dg": 5,  "b": 1, "gth": 15, "nf": 4}
 
     assert already_sorted_dict == utils.sorted_dict(un_sorted_dict)
+
+
+def test_get_key():
+    dict_for_test = {"b": 1, "nf": 4, "dg": 5, "tye": 7, "gth": 15}
+    assert "nf" == utils.get_key(dict_for_test, 4)
